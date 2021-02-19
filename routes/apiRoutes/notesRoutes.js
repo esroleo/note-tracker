47b5260332593b17f4b4ca8f47f283a278404188
esroleo <esroleo@gmail.com>
2021-02-18 21:33:21 -0500

    render sample note in page added

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,10 +1,7 @@
 // ** midleware decalation for routes *** //
-
 const router = require('express').Router();
 
 //const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
-const { filterByQuery } = require('../../lib/notes');
-//const { db } = require('../../data/db.json');
 const { notes } = require('../../db/db');
 
 //console.log(notes)
@@ -14,23 +11,7 @@ router.get('/notes', (req, res) => {
   //res.send("Hello")
 });
 
-
-  // // // // *** Route added*** Return all animals //
-  // router.get('/notes', (req, res) => {
-  //   //res.send('Hello!');
-  //   let results = dbData; //store all json data from our our animals.json
-  //   // console.log(req.query)
-  //   // res.json(results);
-  //   //res.json(animals);
-  //   if (req.query) {
-  //       results = filterByQuery(req.query, results);
-  //     }
-  //     res.json(results);
-  // });
-
-
-
-  module.exports  = router;
+module.exports  = router;
 
 
 
