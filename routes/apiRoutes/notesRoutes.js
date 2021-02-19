f7be440ca1d2f0df8028cfc1fb9681d2a2b2e10b
esroleo <esroleo@gmail.com>
2021-02-18 21:59:35 -0500

    note now saves when clicking the save button

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 
 //const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db');
 
 //console.log(notes)
@@ -11,7 +12,27 @@ router.get('/notes', (req, res) => {
   //res.send("Hello")
 });
 
+router.post('/notes', (req, res) => {
+  // req.body is where our incoming content will be
+  // lenght vs index.. lenght is +1 hence why the below works
+  req.body.id = notes.length.toString();
+  //console.log(req.body);
+
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+   
+  // // if any data in req.body is incorrect, send 400 error back
+  // if (!validateNote(req.body)) {
+  //   res.status(400).send('The note is not properly formatted.');
+  // } else {
+  // // add animal to json file and animals array in this function
+  // const note = createNewNote(req.body, animals);
+  // res.json(note);
+  // //res.json(req.body);
+  // }
+});
+
+
+
 module.exports  = router;
-
-
 
